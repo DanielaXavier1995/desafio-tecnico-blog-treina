c474e4119d67a69ba3bf1ad5f2b170d069949d28
Daniela Xavier <danielamxsoares@gmail.com>
2023-09-09 13:33:09 -0300

    feat: criar estrutura do projeto inicial

---
+++ treina/src/main/java/com/blog/api/treina/TreinaApplication.java
@@ -0,0 +1,13 @@
+package com.blog.api.treina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinaApplication.class, args);
+	}
+
+}
